b4b391b87915c173337f210f33026da0669bc4bf
PARAG-JIRANGE <105792957+PARAG-JIRANGE@users.noreply.github.com>
2022-08-23 19:19:32 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "ctl-terraform-state-file1"
+    bucket         = "jenkine-practice"
     key            = "ec2/demo"
     region         = "ap-south-1"
     dynamodb_table = "tf-lock-state"
